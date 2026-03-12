7a3094fe3dd3fcaccbb00a2fbecf3c2171dc1ba0
2sunyoung <can75607560@gmail.com>
2026-03-12 23:06:45 +0900

    Create 4_con.cpp

---
+++ ch02/ch02-1/4_con.cpp
@@ -0,0 +1,12 @@
+# Title : 축하 메시지 출력 프로그램
+# Author : Lee sunyoung
+# Data : 26.3.12
+
+#include <iostream>
+using namespace std;
+int main() {
+   cout << "군산대학교 \"홍길동\"" << endl;
+   cout << "\"축하합니다.\"" << endl;
+   cout << "100% \\취업율\\";
+   return 0;
+}
