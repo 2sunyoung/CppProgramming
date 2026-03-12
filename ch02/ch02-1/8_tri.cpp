a82a21dbb2ed7e2cb5e52e38f9f68219f5d61ad1
2sunyoung <can75607560@gmail.com>
2026-03-12 23:27:43 +0900

    Update 8_tri.cpp

--- ch02/ch02-1/8_tri.cpp
+++ ch02/ch02-1/8_tri.cpp
@@ -6,8 +6,8 @@
 using namespace std;
 int main() {
    int a = 4;
-   for (int i = 0; i < a; i++) { // 세로
-      for (int j = 0; j < a; j++) {// 가로
+   for (int i = 0; i < a; i++) { 
+      for (int j = 0; j < a; j++) {
          if (i>j-1)
             cout << "*";
          else cout << "  ";
