5e02d10586f6f4d5f325174ebfdd8dbb276bc42d
2sunyoung <can75607560@gmail.com>
2026-03-12 23:12:53 +0900

    Create 6_xstar.cpp

---
+++ ch02/ch02-1/6_xstar.cpp
@@ -0,0 +1,19 @@
+# Title : *을 x형태로 출력하는 프로그램
+# Author : Lee sunyoung
+# Data : 26.3.12
+
+#include <iostream>
+using namespace std;
+int main() {
+   int a=7;
+   for (int i = 0; i < a; i++) { 
+      for (int j = 0; j < a; j++) {
+         if (i == j || j == a - i-1)
+            cout << "* ";
+         else
+            cout << "  ";
+      }
+      cout << endl;
+   }
+   return 0;
+}
