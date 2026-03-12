e352bc81d57aeeb7f05b3772e70f29c3c5c766d7
2sunyoung <can75607560@gmail.com>
2026-03-13 01:43:33 +0900

    Create 8_space.cpp

---
+++ ch02/ch02-2/8_space.cpp
@@ -0,0 +1,20 @@
+# Title : 문자열을 알파벳단위로 분리하는 프로그램
+# Author : Lee sunyoung
+# Data : 26.3.12
+
+#include <iostream>
+using namespace std;
+int main(void) {
+	char text[100];
+
+	cout << "빈칸 없이 문자열을 입력하세요>>";
+	cin >> text;
+
+	char* p = text;
+
+	while (*p != '\0') {
+		cout << *p << ' ';
+		p++;
+	}
+	return 0;
+}
