da769b4a2f949866fb4a444612bca91db1f68d12
2sunyoung <can75607560@gmail.com>
2026-03-13 02:39:13 +0900

    Create 10_wordcnt.cpp

---
+++ ch02/ch02-2/10_wordcnt.cpp
@@ -0,0 +1,28 @@
+# Title : 단어의 개수를 세는 프로그램
+# Author : Lee sunyoung
+# Data : 26.3.12
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main(void) {
+	char text[999];
+
+	while (true) {
+		cout << "영문 텍스트를 입력하세요(빈칸 포함 가능)>>";
+		cin.getline(text, sizeof(text), '\n');
+
+		if (strcmp(text, "exit") == 0) break;
+
+		int cnt = 0;
+		for (int i = 0; text[i] != '\0'; i++) {
+			if (text[i] != ' ' && (text[i + 1] == ' '))
+				cnt++;
+		}
+		if (strcmp(text, "") == 0)
+			cout << "단어의 개수는 " << cnt << "개이다." << endl;
+		else
+			cout << "단어의 개수는 " << cnt + 1 << "개이다." << endl;
+	}
+	return 0;
+}
