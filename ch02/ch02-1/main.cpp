e0363edfcc7aad782d53919c8e436d3ea4a2286c
2sunyoung <can75607560@gmail.com>
2026-03-12 22:39:33 +0900

    Create main.cpp

---
+++ ch02/ch02-1/main.cpp
@@ -0,0 +1,13 @@
+# Title : 개인정보 출력 프로그램
+# Author : Lee sunyoung
+# Data : 26.3.12
+
+#include <iostream>
+using namespace std;
+int main(void) {
+   cout << "이름 : 홍길동" << endl;
+   cout << "주소 : 군산시 대학로 558" << endl;
+   cout << "학번 : 201012" << endl;
+   cout << "차종 : 벤츠";
+   return 0;
+}
