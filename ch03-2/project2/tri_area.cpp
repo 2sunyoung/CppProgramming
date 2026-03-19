4f4ddf0a3ead9556c8e7d210621965e4e568b697
2sunyoung <can75607560@gmail.com>
2026-03-19 17:33:25 +0900

    Create tri_area.cpp

---
+++ ch03-2/project2/tri_area.cpp
@@ -0,0 +1,23 @@
+# Title : 삼각형 면적 계산 프로그램
+# Author : Lee sunyoung
+# Data : 26.3.16
+
+#include <iostream>
+using namespace std;
+class Triangle {
+	double s, h;
+public:
+	Triangle();
+	Triangle(double a, double b);
+	double getArea() { return s * h * 0.5; }
+};
+Triangle::Triangle() : Triangle(1, 1) {}
+Triangle::Triangle(double a, double b) : s(a), h(b) {}
+
+int main() {
+	Triangle tri1;
+	cout << "삼각형의 면적은 " << tri1.getArea() << endl;
+	Triangle tri2(2, 4);
+	cout << "삼각형의 면적은 " << tri2.getArea() << endl;
+	return 0;
+}
