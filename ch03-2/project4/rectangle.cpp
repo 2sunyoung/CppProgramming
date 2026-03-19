3fe3ae6c5b173b40a84dc634a5b9a0f5ea3d14b2
2sunyoung <can75607560@gmail.com>
2026-03-20 00:17:12 +0900

    Update rectangle.cpp

--- ch03-2/project4/rectangle.cpp
+++ ch03-2/project4/rectangle.cpp
@@ -1,22 +1,46 @@
-# Title : 삼각형 면적 계산 프로그램
+# Title : 사각형 정보 출력 프로그램
 # Author : Lee sunyoung
-# Data : 26.3.16
+# Data : 26.3.19
 
 #include <iostream>
 using namespace std;
-class Triangle {
+class Rectangle {
 public:
-	double s;
-	double h;
-	double getArea();
+	int x, y;
+	int x2, y2;
+	int width, height;
+
+	Rectangle();
+	Rectangle(int a, int b);
+	Rectangle(int a, int b, int w, int h);
+	
+	int getArea() { return width * height; }
+	int getPerimeter() { return 2 * (width + height); }
+	void getXY();
+	bool isSquare();
 };
-double Triangle::getArea() {
-	return s * h * 0.5;
+Rectangle::Rectangle() : Rectangle(1, 1, 1, 1) {}
+Rectangle::Rectangle(int a, int b) : Rectangle(a, b, 1, 1) {}
+Rectangle::Rectangle(int a, int b, int w, int h) : x(a), y(b), width(w), height(h) {}
+
+void Rectangle::getXY() {
+	x2 = x + width;
+	y2 = y - height;
 }
+bool Rectangle::isSquare() {
+	if (width == height) return true;
+	else return false;
+}
+
 int main() {
-	Triangle t;
-	t.s = 3.0;
-	t.h = 5.0;
-	cout << "삼각형의 면적은 " << t.getArea();
+	Rectangle rect1;
+	Rectangle rect2(3, 5); 
+	Rectangle rect3(3, 5, 2, 4);
+	
+	cout << "rect1의 면적은 " << rect1.getArea() << endl;
+	cout << "rect2의 둘레길이는 " << rect2.getPerimeter() << endl;
+
+	rect3.getXY();
+	cout << "rect3의 우측하단의 좌표는 (" << rect3.x2 << "," << rect3.y2 << ")" << endl;
 	return 0;
 }
