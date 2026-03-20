fd8b6edee0ea4a51fe902ae8b49466d2a5c33043
2sunyoung <can75607560@gmail.com>
2026-03-20 13:56:45 +0900

    Update rectangle.cpp

--- ch03-2/project4/rectangle.cpp
+++ ch03-2/project4/rectangle.cpp
@@ -21,9 +21,7 @@ public:
 };
 Rectangle::Rectangle() : Rectangle(1, 1, 1, 1) {}
 Rectangle::Rectangle(int a, int b) : Rectangle(a, b, 1, 1) {}
-Rectangle::Rectangle(int a, int b, int w, int h) : x(a), y(b), width(w), height(h) {
-	getXY();
-}
+Rectangle::Rectangle(int a, int b, int w, int h) : x(a), y(b), width(w), height(h) { getXY(); }
 
 void Rectangle::getXY() {
 	x2 = x + width;
