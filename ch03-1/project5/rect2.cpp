71b99e47921ce92b85f63af4cec313aacd7889dc
2sunyoung <can75607560@gmail.com>
2026-03-22 22:58:48 +0900

    Update rect2.cpp

--- ch03-1/project5/rect2.cpp
+++ ch03-1/project5/rect2.cpp
@@ -1,6 +1,6 @@
-# Title : 사각형 정보 계산 프로그램
-# Author : Lee sunyoung
-# Data : 26.3.16
+// Title : 사각형 정보 계산 프로그램
+// Author : Lee sunyoung
+// Data : 26.3.16
 
 #include <iostream>
 using namespace std;
