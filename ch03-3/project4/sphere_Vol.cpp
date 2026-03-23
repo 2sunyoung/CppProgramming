e261c97f57e6deed375415115c02479d2f62fdab
2sunyoung <can75607560@gmail.com>
2026-03-23 18:11:39 +0900

    Create sphere_Vol.cpp

---
+++ ch03-3/project4/sphere_Vol.cpp
@@ -0,0 +1,32 @@
+// Title : 구의 부피 계산 프로그램 (생성과 소멸 메시지 출력)
+// Author : Lee sunyoung
+// Data : 26.3.23
+
+#include <iostream>
+using namespace std;
+class Sphere {
+public:
+	double r;
+	Sphere();
+	Sphere(double a);
+	~Sphere();
+	double getVolume();
+};
+Sphere::Sphere(): Sphere(1) {}
+Sphere::Sphere(double a): r(a) {
+	cout << "반지름" << r << "인 구 생성" << endl;
+}
+Sphere::~Sphere(){
+	cout << "반지름" << r << "인 구 소멸" << endl;
+}
+double Sphere::getVolume() {
+	return (4.0) / (3.0) * 3.14 * r * r * r;
+}
+int main() {
+	Sphere sph1; 
+	cout << "구의 부피는" << sph1.getVolume() << endl;
+	Sphere sph2(3);
+	cout << "구의 부피는" << sph2.getVolume() << endl;
+
+	return 0;
+}
