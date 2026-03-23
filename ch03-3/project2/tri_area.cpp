c5c3f27162d1d34f51e2ae3ade7cdae520c609d5
2sunyoung <can75607560@gmail.com>
2026-03-23 16:05:22 +0900

    Create tri_area.cpp

---
+++ ch03-3/project2/tri_area.cpp
@@ -0,0 +1,35 @@
+// Title : 삼각형 면적 계산 프로그램 (생성과 소멸 메시지 출력)
+// Author : Lee sunyoung
+// Data : 26.3.23
+
+#include <iostream>
+using namespace std;
+class Triangle {
+public:
+	double s, h;
+	Triangle();
+	Triangle(double a, double b);
+	~Triangle();
+	double getArea();
+};
+
+Triangle::Triangle(): Triangle(1,1) {}
+Triangle::Triangle(double a, double b) : s(a), h(b) {
+	cout << "밑변" << s << " 높이" << h << "인 삼각형 생성" << endl;
+}
+Triangle::~Triangle() {
+	cout << "밑변" << s << " 높이" << h << "인 삼각형 소멸" << endl;
+}
+
+double Triangle::getArea() {
+	return s * h * 0.5;
+}
+
+int main() {
+	Triangle tri1;
+	cout << "삼각형의 면적은 " << tri1.getArea() << endl;
+
+	Triangle tri2(2, 4);
+	cout << "삼각형의 면적은 " << tri2.getArea() << endl;
+	return 0;
+}
