473c9622ab91f9978a50abe4cd899ad8ccbe66a8
2sunyoung <can75607560@gmail.com>
2026-04-07 23:19:57 +0900

    Update dynamic_str.cpp

--- ch04-2/project5/dynamic_str.cpp
+++ ch04-2/project5/dynamic_str.cpp
@@ -1,1 +1,27 @@
+// Title : 동적 할당) 입력받은 문자열을 출력하는 프로그램
+// Author : Lee sunyoung
+// Data : 26.4.7
 
+#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	cout << "저장할 문자열의 크기를 입력하세요: ";
+	cin >> n;
+	if (n <= 0) return -1;
+
+	char* str = new char[n + 1];
+	if (!str) {
+		cout << "메모리를 할당할 수 없습니다.";
+		return -1;
+	}
+
+	cin.ignore();
+	cout << "문자열을 입력하시오: ";
+	cin.getline(str,n+1);
+
+	cout << "입력한 문자열은 " << str << "입니다." << endl;
+
+	delete[] str;
+	return 0;
+}
