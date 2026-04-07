e37bf0f3230e74e0d39c0ec2204a049e9c13928d
2sunyoung <can75607560@gmail.com>
2026-04-07 19:38:51 +0900

    Update dynamic_avg.cpp

--- ch04-2/project3/dynamic_avg.cpp
+++ ch04-2/project3/dynamic_avg.cpp
@@ -1,32 +1,29 @@
-// Title : 객체 배열과 객체 포인터를 이용한 삼각형 면적 계산 프로그램
+// Title : 동적 할당을 이용한 평균값 계산 프로그램
 // Author : Lee sunyoung
-// Data : 26.4.2
+// Data : 26.4.7
 
 #include <iostream>
 using namespace std;
-class Triangle {
-private:
-	int width, height;
-public:
-	Triangle(int w, int h) : width(w), height(h) {}
-	~Triangle() {}
-	double getArea() { return width * height * 0.5; }
-};
 int main() {
-	Triangle triArray[3] = { Triangle(2,2) , Triangle(4,4) , Triangle(6,6) };
-	Triangle* tp = triArray;
-	for (int i = 0; i < 3; i++) 
-		cout << "삼각형" << i << "의 면적은 " << (triArray + i)->getArea() << endl;
-	cout << endl;
-	for (int i = 0; i < 3; i++)
-		cout << "삼각형" << i << "의 면적은 " << (tp+i)->getArea() << endl;
-	cout << endl;
-	for (int i = 0; i < 3; i++)
-		cout << "삼각형" << i << "의 면적은 " << tp[i].getArea() << endl;
-	cout << endl;
-	for (int i = 0; i < 3; i++) {
-		cout << "삼각형" << i << "의 면적은 " << tp->getArea() << endl;
-		tp++;
+	int n;
+	double sum = 0;
+	cout << "입력할 정수의 개수를 입력하세요: ";
+	cin >> n;
+	if (n <= 0) return -1;
+
+	int* p = new int[n];
+	if (!p) {
+		cout << "메모리를 할당할 수 없습니다.";
+		return -1;
 	}
+
+	cout << n << "개의 정수를 입력 하시오." << endl;
+	for (int i = 0; i < n; i++) {
+		cin >> p[i];
+		sum += p[i];
+	}
+	cout << "평균값은 " << sum / n << "입니다." << endl;
+
+	delete[] p;
 	return 0;
 }
