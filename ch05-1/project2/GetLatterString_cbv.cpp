fc94d80b3b8ab13796b5af1771cf5fe10a153b1f
2sunyoung <can75607560@gmail.com>
2026-05-06 17:28:48 +0900

    Update GetLatterString_cbv.cpp

--- ch05-1/project2/GetLatterString_cbv.cpp
+++ ch05-1/project2/GetLatterString_cbv.cpp
@@ -10,7 +10,7 @@ int main() {
 	string s1("hello");
 	string s2("world");
 	string res;
-	res = GetLatterString(s1, s2); // 함수 호출
+	res = GetLatterString(s1, s2); 
 	cout << "사전에서 뒤에 나오는 문자열은 " << res << "입니다." << endl;
 }
 string GetLatterString(string s1, string s2) {
