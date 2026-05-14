c81dfec9bfe94ec6327cfd905eac06cc91c0774b
2sunyoung <can75607560@gmail.com>
2026-05-14 23:32:56 +0900

    Create change_name.cpp

---
+++ ch05-4/project2/change_name.cpp
@@ -0,0 +1,36 @@
+// Title : 이름 바꾸기 프로그램 (string 클래스 사용)
+// Author : Lee sunyoung
+// Data : 26.5.14
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+using namespace std;
+class Person {
+	string name;
+	int id;
+public:
+	Person(int id, string name);
+	void changeName(string name);
+	void show() { cout << id << ',' << name << endl; }
+};
+Person::Person(int id, string name) { 
+	this->id = id; 
+	this->name = name;
+}
+void Person::changeName(string name) {
+	if (name.length() > this->name.length()) return;
+	this->name = name;
+}
+int main() {
+	Person father(1, "Kitae"); 
+	Person daughter(father); 
+	cout << "daughter 객체 생성 직후 ----" << endl;
+	father.show(); 
+	daughter.show(); 
+	daughter.changeName("Grace"); 
+	cout << "daughter 이름을 Grace로 변경한 후 ----" << endl;
+	father.show(); 
+	daughter.show();
+	return 0;
+}
