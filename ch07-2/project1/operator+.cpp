b396f164cbe0aba0f4a5cd3b637bc60e24e9b0c7
2sunyoung <can75607560@gmail.com>
2026-06-01 10:29:30 +0900

    Create operator+.cpp

---
+++ ch07-2/project1/operator+.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+class Complex {
+	int real, img;
+public:
+	Complex(int r = 0, int i = 0):real(r), img(i) {}
+	void show();
+	Complex operator+ (Complex op2);
+};
+void Complex::show() {
+	if (img < 0) cout << real << img << "j";
+	else cout << real << "+" << img << "j";
+}
+Complex Complex::operator+ (Complex op2) {
+	Complex res;
+	res.real = this->real + op2.real;
+	res.img = this->img + op2.img;
+	return res;
+}
+int main() {
+	Complex x(2, 3), y(-5, 10), sum;
+	sum = x.operator+ (y);
+	cout << "두 복소수의 합은 ";
+	sum.show();
+	return 0;
+}
